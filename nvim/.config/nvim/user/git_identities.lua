a550b3878ec0e7eaf0c31098e975eeb0e8a7f6bd
arafays <me@arafays.com>
2025-02-27 20:56:52 +0500

    update

---
+++ nvim/.config/nvim/user/git_identities.lua
@@ -0,0 +1,8 @@
+return {
+  { email = "dev.testing785@gmail.com", name = "motionbees-developer" },
+  { email = "dev.testing785@gmail.com", name = "developer" },
+  { email = "dev.testing785@gmail.com", name = "Webnhubs" },
+  { email = "abdul.rafay@mayabytes.com", name = "Abdul Rafay Shaikh" },
+  { email = "me@arafays.com", name = "arafays" },
+  { email = "agency@craftidesigns.com", name = "Mohammad Osama" },
+}
