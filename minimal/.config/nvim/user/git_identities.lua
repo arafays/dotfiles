f7d27be33fb620234e426a4886217f291c7d9931
Abdul Rafay Shaikh <me@arafays.com>
2025-04-20 19:30:50 +0500

    for the grub

--- minimal/.config/nvim/user/git_identities.lua
+++ minimal/.config/nvim/user/git_identities.lua
@@ -1,9 +1,8 @@
 return {
+  { name = "Abdul Rafay Shaikh", email = "abdul.rafay@mayabytes.com", signingkey = "" },
+  { name = "Abdul Rafay Shaikh", email = "me@arafays.com", signingkey = "180E3C70D183B913" },
   { name = "motionbees-developer", email = "dev.testing785@gmail.com", signingkey = "" },
   { name = "developer", email = "dev.testing785@gmail.com", signingkey = "" },
   { name = "Webnhubs", email = "dev.testing785@gmail.com", signingkey = "" },
-  { name = "Abdul Rafay Shaikh", email = "abdul.rafay@mayabytes.com", signingkey = "" },
-  { name = "Abdul Rafay Shaikh", email = "me@arafays.com", signingkey = "180E3C70D183B913" },
   { name = "Mohammad Osama", email = "agency@craftidesigns.com", signingkey = "" },
-  { name = "Abdul Rafay Shaikh", email = "me@arafays.com", signingkey = "" },
 }
--- minimal/.zprofile
+++ minimal/.zprofile
@@ -1,12 +1,9 @@
 # Group completion initializations into a single function
 function init_completions() {
   [[ -x "$(command -v fzf)" ]] && eval "$(fzf --zsh)"
-  [[ -x "$(command -v warp-cli)" ]] && eval "$(warp-cli generate-completions zsh)"
-  [[ -x "$(command -v pnpm)" ]] && eval "$(pnpm completion zsh)"
   [[ -x "$(command -v gh)" ]] && eval "$(gh copilot alias zsh)"
   [[ -x "$(command -v zoxide)" ]] && eval "$(zoxide init zsh)"
-  [[ -x "$(command -v go-blueprint)" ]] && eval "$(go-blueprint completion zsh)"
-  [[ -x "$(command -v mise)" ]] && { eval "$(mise activate zsh)"; eval "$(mise completion zsh)"; }
+  [[ -x "$(command -v mise)" ]] && { eval "$(mise activate zsh)";}
 }
 init_completions
 
