9c49bd779a8ae7080b52a3784d24a07acca64321
arafays <me@arafays.com>
2025-12-20 19:01:20 +0500

    up

---
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+-- better exit insert mode
+map("i", "jk", "<Esc>", opts)
+map("i", "jj", "<Esc>", opts)
+
+-- to the middle when pressing ctrl + u
+map("n", "<C-u>", "<C-u>zz", opts)
+map("n", "<C-d>", "<C-d>zz", opts)
+
+--INFO: find and center
+map("n", "n", "nzzzv", opts)
+map("n", "N", "Nzzzv", opts)
---
+++ private_dot_config/nvim/user/git_identities.lua
@@ -0,0 +1,8 @@
+return {
+  { name = "motionbees-developer", email = "dev.testing785@gmail.com", signingkey = "" },
+  { name = "developer", email = "dev.testing785@gmail.com", signingkey = "" },
+  { name = "Webnhubs", email = "dev.testing785@gmail.com", signingkey = "" },
+  { name = "arafays", email = "me@arafays.com", signingkey = "" },
+  { name = "Mohammad Osama", email = "agency@craftidesigns.com", signingkey = "" },
+  { name = "Abdul Rafay Shaikh", email = "abdul.rafay@mayabytes.com", signingkey = "" },
+}
