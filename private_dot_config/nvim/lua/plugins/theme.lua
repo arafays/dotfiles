0832a4ec96958b42a7da933bc51edc2a6d534c5c
arafays <me@arafays.com>
2026-05-30 23:06:11 +0500

    fix

--- private_dot_config/nvim/lua/plugins/theme.lua
+++ private_dot_config/nvim/lua/plugins/theme.lua
@@ -1,29 +1,34 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		lazy = false,
-		opts = {
-			transparent = true,
-			style = "night", -- Options: "storm", "moon", "day", "night"
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			-- Dark mode
-			colorscheme = "tokyonight",
-		},
-	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	priority = 1000,
+	-- 	lazy = false,
+	-- 	opts = {
+	-- 		transparent = true,
+	-- 		style = "night", -- Options: "storm", "moon", "day", "night"
+	-- 		styles = {
+	-- 			sidebars = "transparent",
+	-- 			floats = "transparent",
+	-- 		},
+	-- 	},
+	-- },
+	-- {
+	-- 	"LazyVim/LazyVim",
+	-- 	opts = {
+	-- 		-- Dark mode
+	-- 		colorscheme = "tokyonight",
+	-- 	},
+	-- },
 	-- {
 	--   "crusoexia/vim-monokai",
 	--   priority = 1000, -- ensure it loads before LazyVim
 	--   lazy = false,
 	-- },
+	--
+	"RRethy/base16-nvim",
+	config = function()
+		require("matugen").setup()
+	end,
 }
 
 -- To live preview and switch themes, use :Telescope colorscheme
